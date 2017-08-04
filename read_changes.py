f7c8a9e629d954c7bbff03c471aa3d99695a7ab9
dasilva1996 <idiamin_dasilva@yahoo.com>
2017-08-04 16:47:55 +0100

    CA5
    CA5

---
+++ read_changes.py
@@ -0,0 +1,29 @@
+
+changes_file = 'changes_python.log'
+data = [line.strip() for line in open(changes_file, 'r')]
+print(len(data))
+
+sep = 72*'-'
+
+commits = []
+index = 0
+while index < len(data):
+    try:
+        details = data[index + 1].split('|')
+        commits.append(details)
+        index = data.index(sep, index + 1)
+    except IndexError:
+        break
+			
+
+
+output_file = "changes.csv"
+my_file = open(output_file, "w")
+my_file.write("Revision, Author,Date\n")
+for details in commits:
+    my_file.write(details[0] + ',' + 
+	        details[1] + ',' +
+			details[2] + '\n')
+my_file.close()	
+	
+	
